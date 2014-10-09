903f16f15670898b22333d705dc95e682a8466b3
Pavel Strashnov <Pavel_Strashnov@EPRUSARW1075.saratov.epam.com>
2014-10-09 12:54:00 +0400

    Initial commit

---
+++ angular-http-transform-date.js
@@ -0,0 +1,85 @@
+'use strict';
+
+angular.module('angular-http-transform-date', []).factory('HttpTransformDate', [
+    function () {
+        var onlyDateFormat = 'YYYY-MM-DD';
+
+        function traverseProperties(data, properties, modifier) {
+            angular.forEach(properties, function (prop) {
+                transformProperty(data, prop);
+            });
+
+            function transformProperty(data, prop) {
+                if (angular.isArray(data)) {
+                    angular.forEach(data, function (dataItem) {
+                        transformProperty(dataItem, prop);
+                    });
+                }
+
+                var propFormat = /^(\w+?)((\.\w+)*?)$/g,
+                    match = propFormat.exec(prop),
+                    topProp = match && match[1],
+                    subProp = match && match[2] && match[2].substring(1);
+
+                if (!topProp) {
+                    return;
+                }
+
+                if (subProp) {
+                    if (angular.isObject(data[topProp])) {
+                        transformProperty(data[topProp], subProp);
+                    }
+                } else {
+                    if (data[topProp]) {
+                        modifier(data, topProp);
+                    }
+                }
+            }
+        }
+
+        return {
+            transformToDateOnly: function () {
+                var properties = arguments;
+
+                return function (data) {
+
+                    traverseProperties(data, properties, function (data, prop) {
+                        var date = moment(data[prop]);
+                        if (data[prop] && date.isValid()) {
+                            data[prop] = date.format(onlyDateFormat);
+                        }
+                    });
+
+                    return angular.toJson(data);
+                };
+            },
+            getDateOnly: function (date) {
+                if (!date) {
+                    return null;
+                }
+                date = moment(date);
+                return date.isValid() ? date.format(onlyDateFormat) : null;
+            },
+
+            transformFromString: function () {
+                var properties = arguments;
+
+                return function (data) {
+                    try {
+                        var parsed = angular.fromJson(data);
+                        data = parsed;
+                    } catch (e) {
+                    }
+                    traverseProperties(data, properties, function (data, prop) {
+                        var date = new Date(data[prop]);
+                        if (data[prop] && date) {
+                            data[prop] = date;
+                        }
+                    });
+
+                    return data;
+                };
+            }
+        };
+    }
+]);
---
+++ tests/angular-http-transform-date.spec.js
@@ -0,0 +1,164 @@
+'use strict';
+
+describe('Transform date service', function () {
+
+    var HttpTransformDate,
+        onlyDateFormat = 'YYYY-MM-DD';
+
+    beforeEach(module('angular-http-transform-date'));
+
+    beforeEach(inject(function (_HttpTransformDate_) {
+        HttpTransformDate = _HttpTransformDate_;
+    }));
+
+    it('should be defined', function () {
+        //Assert
+        expect(HttpTransformDate).toBeDefined();
+    });
+
+    it('should be able to transform date variable to date format', function () {
+        //Arrange
+        var date = new Date();
+
+        //Act
+        var transformed = HttpTransformDate.getDateOnly(date);
+
+        //Assert
+        expect(transformed).toEqual(moment(date).format(onlyDateFormat));
+    });
+
+    it('should be able to transform object property to date format', function () {
+        //Arrange
+        var date = new Date(),
+            data = {
+                date: date
+            };
+
+        //Act
+        var transformed = HttpTransformDate.transformToDateOnly('date')(data);
+        data.date = moment(date).format(onlyDateFormat);
+
+        //Assert
+        expect(transformed).toEqual(angular.toJson(data));
+    });
+
+    it('should be able to transform object property to date from string in date format', function () {
+        //Arrange
+        var date = new Date(),
+            dateString = moment(date).format(onlyDateFormat),
+            data = {
+                date: dateString
+            };
+
+        //Act
+        var transformed = HttpTransformDate.transformFromString('date')(data);
+
+        //Assert
+        expect(transformed.date).toEqual(new Date(dateString));
+    });
+
+    it('should be able to transform object sub property to date format', function () {
+        //Arrange
+        var date = new Date(),
+            data = {
+                subProperty: {
+                    date: date
+                }
+            };
+
+        //Act
+        var transformed = HttpTransformDate.transformToDateOnly('subProperty.date')(data);
+        data.subProperty.date = moment(date).format(onlyDateFormat);
+
+        //Assert
+        expect(transformed).toEqual(angular.toJson(data));
+    });
+
+    it('should be able to transform complex object with sub properties to date format', function () {
+        //Arrange
+        var date = new Date(),
+            data = {
+                subProperty: {
+                    subSubProperty: {
+                        date: date
+                    }
+                }
+            };
+
+        //Act
+        var transformed = HttpTransformDate.transformToDateOnly('subProperty.subSubProperty.date')(data);
+        data.subProperty.subSubProperty.date = moment(date).format(onlyDateFormat);
+
+        //Assert
+        expect(transformed).toEqual(angular.toJson(data));
+    });
+
+    it('should be able to transform array of objects with date field to date format', function () {
+        //Arrange
+        var date = new Date(),
+            data = [
+                { date: date },
+                { date: date }
+            ];
+
+        //Act
+        var transformed = HttpTransformDate.transformToDateOnly('date')(data);
+        for (var i = 0; i < data.length; i++) {
+            data[i].date = moment(date).format(onlyDateFormat);
+        }
+
+        //Assert
+        expect(transformed).toEqual(angular.toJson(data));
+    });
+
+    it('should be able to transform object with sub property as an array with date field to date format', function () {
+        //Arrange
+        var date = new Date(),
+            data = {
+                subProperty: [
+                    { date: date },
+                    { date: date }
+                ]
+            };
+
+        //Act
+        var transformed = HttpTransformDate.transformToDateOnly('subProperty.date')(data);
+        for (var i = 0; i < data.subProperty.length; i++) {
+            data.subProperty[i].date = moment(date).format(onlyDateFormat);
+        }
+
+        //Assert
+        expect(transformed).toEqual(angular.toJson(data));
+    });
+
+    it('should be able to transform array of objects with sub property as an array with date field to date format', function () {
+        //Arrange
+        var date = new Date(),
+            formattedDate = moment(date).format(onlyDateFormat),
+            data = [
+                {
+                    subProperty: [
+                        { date: date },
+                        { date: date }
+                    ]
+                },
+                {
+                    subProperty: [
+                        { date: date },
+                        { date: date }
+                    ]
+                }
+            ];
+
+        //Act
+        var transformed = HttpTransformDate.transformToDateOnly('subProperty.date')(data);
+        for (var i = 0; i < data.length; i++) {
+            for (var j = 0; j < data[i].subProperty.length; j++) {
+                data[i].subProperty[j].date = formattedDate;
+            }
+        }
+
+        //Assert
+        expect(transformed).toEqual(angular.toJson(data));
+    });
+});
